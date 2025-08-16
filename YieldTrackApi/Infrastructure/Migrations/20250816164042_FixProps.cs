2427fb63795624ed338b9f1564cd5bb4ece5029e
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-08-16 13:46:57 -0300

    feat: migrations

---
+++ YieldTrackApi/Infrastructure/Migrations/20250816164042_FixProps.cs
@@ -0,0 +1,40 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FixProps : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "code",
+                table: "FixedIncomeAssets",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+
+            migrationBuilder.AddColumn<string>(
+                name: "rate_type",
+                table: "FixedIncomeAssets",
+                type: "longtext",
+                nullable: false)
+                .Annotation("MySql:CharSet", "utf8mb4");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "code",
+                table: "FixedIncomeAssets");
+
+            migrationBuilder.DropColumn(
+                name: "rate_type",
+                table: "FixedIncomeAssets");
+        }
+    }
+}
