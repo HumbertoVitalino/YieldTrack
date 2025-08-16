b3a7aaffd0d570c5dc6cd5bcf901e9020cf24f83
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-08-16 13:46:21 -0300

    feat: infrasctructure database models

---
+++ YieldTrackApi/Infrastructure/Repositories/Models/PriceHistory.cs
@@ -0,0 +1,39 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Infrastructure.Repositories.Models;
+
+[Table("PriceHistory")]
+public class PriceHistory : Model
+{
+    [Required]
+    public Guid AssetId { get; private set; }
+
+    [ForeignKey(nameof(AssetId))]
+    public FixedIncomeAsset Asset { get; private set; } = default!;
+
+    [Required]
+    public DateTime ReferenceDate { get; private set; }
+
+    [Required]
+    public decimal Price { get; private set; }
+
+    [Required]
+    public decimal Rate { get; private set; }
+
+    protected PriceHistory() { }
+
+    public PriceHistory(
+        Guid id,
+        DateTime referenceDate,
+        decimal price,
+        decimal rate,
+        DateTime createdAt,
+        DateTime updatedAt
+    ) : base(id, createdAt, updatedAt)
+    {
+        ReferenceDate = referenceDate;
+        Price = price;
+        Rate = rate;
+    }
+}
---
+++ YieldTrackApi/Infrastructure/Repositories/Models/UserInvestment.cs
@@ -0,0 +1,43 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Infrastructure.Repositories.Models;
+
+public class UserInvestment : Model
+{
+    [Required]
+    public Guid UserId { get; private set; }
+
+    [ForeignKey(nameof(UserId))]
+    public User User { get; private set; } = default!;
+
+    [Required]
+    public Guid AssetId { get; private set; }
+
+    [ForeignKey(nameof(AssetId))]
+    public FixedIncomeAsset Asset { get; private set; } = default!;
+
+    [Required]
+    public decimal Quantity { get; private set; }
+
+    [Required]
+    public DateTime PurchaseDate { get; private set; }
+
+    protected UserInvestment() { }
+
+    public UserInvestment(
+        Guid id,
+        Guid userId,
+        Guid assetId,
+        decimal quantity,
+        DateTime purchaseDate,
+        DateTime createdAt,
+        DateTime updatedAt
+    ) : base(id, createdAt, updatedAt)
+    {
+        UserId = userId;
+        AssetId = assetId;
+        Quantity = quantity;
+        PurchaseDate = purchaseDate;
+    }
+}
