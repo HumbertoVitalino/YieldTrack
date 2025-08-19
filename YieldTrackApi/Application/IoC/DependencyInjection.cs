b97dda66a85a90c76507a172eb1eb9d8ccf8569f
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-08-18 22:00:21 -0300

    feat: validator

---
+++ YieldTrackApi/Application/IoC/DependencyInjection.cs
@@ -0,0 +1,26 @@
+﻿using Application.Behavior;
+using Application.UseCases.CreateUser;
+using FluentValidation;
+using MediatR;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Application.IoC;
+
+[ExcludeFromCodeCoverage]
+public static class DependencyInjection
+{
+    public static IServiceCollection AddMediatr(this IServiceCollection services, IConfiguration configuration)
+    {
+        services.AddMediatR(cfg =>
+        {
+            cfg.RegisterServicesFromAssembly(typeof(CreateUser).Assembly);
+        });
+
+        services.AddValidatorsFromAssembly(typeof(CreateUser).Assembly);
+        services.AddTransient(typeof(IPipelineBehavior<,>), typeof(ValidationPipelineBehavior<,>));
+
+        return services;
+    }
+}
