1793493cf0ac1352b128a266ae197ad5828438a7
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-08-18 22:01:26 -0300

    feat: base repository implementantion with unit of work

---
+++ YieldTrackApi/Infrastructure/Repositories/Repository.cs
@@ -0,0 +1,10 @@
+﻿using Application.Interfaces.Repositories;
+using Domain;
+
+namespace Infrastructure.Repositories;
+
+public abstract class Repository<T>(YieldTrackContext context) : IRepository<T> where T : Entity
+{
+    protected readonly YieldTrackContext _context = context;
+    public IUnitOfWork UnitOfWork => _context;
+}
