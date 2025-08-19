f7db1d385d0cb0bc4a9c20cd9638f07016937803
HumbertoVitalino <130328321+HumbertoVitalino@users.noreply.github.com>
2025-08-19 20:44:10 -0300

    feat: create user usecase boundaries

---
+++ YieldTrackApi/Application/UseCases/CreateUser/Boundaries/CreateUserMapper.cs
@@ -0,0 +1,15 @@
+﻿using Domain;
+
+namespace Application.UseCases.CreateUser.Boundaries;
+
+public static class CreateUserMapper
+{
+    public static User MapToDomain(this CreateUserInput input, byte[] passwordHash, byte[] passwordSalt) =>
+        new(
+            Guid.NewGuid(),
+            input.Name,
+            input.Email,
+            passwordHash,
+            passwordSalt
+        );
+}
