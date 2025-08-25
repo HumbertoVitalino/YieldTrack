dd52303bde79ad29a6987537d0ad12cbcec0b040
humberto <130328321+HumbertoVitalino@users.noreply.github.com>
2025-08-24 22:41:20 -0300

    feat: login user uc boundaries

---
+++ YieldTrackApi/Application/UseCases/LoginUser/Boundaries/LoginUserInput.cs
@@ -0,0 +1,9 @@
+﻿using Application.Commons;
+using MediatR;
+
+namespace Application.UseCases.LoginUser.Boundaries;
+
+public sealed record LoginUserInput(
+    string Email,
+    string Password
+) : IRequest<Output>;
