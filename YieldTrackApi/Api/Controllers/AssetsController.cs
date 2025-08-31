b493d62d13e08745f91ced355feb6775f04c2eab
humberto <130328321+HumbertoVitalino@users.noreply.github.com>
2025-08-30 22:44:53 -0300

    feat: controller

---
+++ YieldTrackApi/Api/Controllers/AssetsController.cs
@@ -0,0 +1,31 @@
+﻿using Application.Commons;
+using Application.UseCases.CreateAsset.Boundaries;
+using MediatR;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Authorize]
+[Route("api/v1/[controller]")]
+public class AssetsController(
+    IMediator mediator    
+) : BaseController
+{
+    private readonly IMediator _mediator = mediator;
+
+    [HttpPost]
+    [ProducesResponseType(typeof(Output), StatusCodes.Status201Created)]
+    [ProducesResponseType(typeof(Output), StatusCodes.Status400BadRequest)]
+    [ProducesResponseType(StatusCodes.Status500InternalServerError)]
+    public async Task<IActionResult> CreateAsync(CreateAssetInput request, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(request, cancellationToken);
+
+        if (!response.IsValid)
+            return BadRequest(response);
+
+        return StatusCode(StatusCodes.Status201Created, response);
+    }
+}
