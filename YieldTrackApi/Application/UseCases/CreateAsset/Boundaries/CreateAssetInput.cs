c8a81bd577ece1cdaada001a408a65ab4a67054b
humberto <130328321+HumbertoVitalino@users.noreply.github.com>
2025-08-30 22:44:05 -0300

    feat: create asset boundaries

---
+++ YieldTrackApi/Application/UseCases/CreateAsset/Boundaries/CreateAssetInput.cs
@@ -0,0 +1,14 @@
+﻿using Application.Commons;
+using MediatR;
+
+namespace Application.UseCases.CreateAsset.Boundaries;
+
+public sealed record CreateAssetInput(
+    string Code,
+    string Name,
+    string Issuer,
+    decimal FaceValue,
+    DateTime MaturityDate,
+    decimal CurrentRate,
+    string RateType
+) : IRequest<Output>;
---
+++ YieldTrackApi/Application/UseCases/CreateAsset/Boundaries/CreateAssetInputValidator.cs
@@ -0,0 +1,32 @@
+﻿using FluentValidation;
+
+namespace Application.UseCases.CreateAsset.Boundaries;
+
+public class CreateAssetInputValidator : AbstractValidator<CreateAssetInput>
+{
+    private const int MinimumValue = 0;
+
+    public CreateAssetInputValidator()
+    {
+        RuleFor(x => x.Code)
+            .NotEmpty().WithMessage("The code is required.");
+
+        RuleFor(x => x.Name)
+            .NotEmpty();
+
+        RuleFor(x => x.Issuer)
+            .NotEmpty();
+
+        RuleFor(x => x.FaceValue)
+            .GreaterThan(MinimumValue).NotEmpty();
+
+        RuleFor(x => x.CurrentRate)
+            .GreaterThan(MinimumValue).NotEmpty();
+
+        RuleFor(x => x.MaturityDate)
+            .GreaterThan(DateTime.Now).NotEmpty();
+
+        RuleFor(x => x.RateType)
+            .NotEmpty();
+    }
+}
