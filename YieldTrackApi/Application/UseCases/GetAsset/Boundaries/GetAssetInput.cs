b1ea994b3cc92264cc9f860962aa11746a34f904
humberto <130328321+HumbertoVitalino@users.noreply.github.com>
2025-08-31 21:37:51 -0300

    feat: get asset uc

---
+++ YieldTrackApi/Application/UseCases/GetAsset/Boundaries/GetAssetInput.cs
@@ -0,0 +1,8 @@
+﻿using Application.Commons;
+using MediatR;
+
+namespace Application.UseCases.GetAsset.Boundaries;
+
+public sealed record GetAssetInput(
+    Guid Id
+) : IRequest<Output>;
---
+++ YieldTrackApi/Application/UseCases/GetAsset/GetAsset.cs
@@ -0,0 +1,30 @@
+﻿using Application.Commons;
+using Application.Interfaces.Repositories;
+using Application.Mappers;
+using Application.UseCases.GetAsset.Boundaries;
+using MediatR;
+
+namespace Application.UseCases.GetAsset;
+
+public class GetAsset(
+    IAssetRepository assetRepository
+) : IRequestHandler<GetAssetInput, Output>
+{
+    private readonly IAssetRepository _assetRepository = assetRepository;
+
+    public async Task<Output> Handle(GetAssetInput input, CancellationToken cancellationToken)
+    {
+        Output output = new();
+
+        var asset = await _assetRepository.GetAsync(input.Id, cancellationToken);
+
+        if (asset is null)
+        {
+            output.AddErrorMessage("Unable to find an asset for this id");
+            return output;
+        }
+
+        output.AddResult(asset.MapToDto());
+        return output;
+    }
+}
