abc0584b0571772f4ef032361ec939343c360b23
humberto <130328321+HumbertoVitalino@users.noreply.github.com>
2025-09-11 22:06:52 -0300

    feat: upsertasync interface

--- YieldTrackApi/Application/Interfaces/Repositories/IAssetRepository.cs
+++ YieldTrackApi/Application/Interfaces/Repositories/IAssetRepository.cs
@@ -6,6 +6,7 @@ public interface IAssetRepository : IRepository<FixedIncomeAsset>
 {
     Task<FixedIncomeAsset?> GetAsync(Guid id, CancellationToken cancellationToken);
     Task<FixedIncomeAsset?> GetAsync(string code, CancellationToken cancellationToken);
+    Task<FixedIncomeAsset> UpsertAsync(FixedIncomeAsset asset, CancellationToken cancellationToken);
     Task<IEnumerable<FixedIncomeAsset>> GetAsync(CancellationToken cancellationToken);
     Task InsertAsync(FixedIncomeAsset asset, CancellationToken cancellationToken);
 }
