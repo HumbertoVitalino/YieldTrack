cd605d60232fedef2783cafc142cfe4d1c9550a3
humberto <130328321+HumbertoVitalino@users.noreply.github.com>
2025-09-14 16:38:57 -0300

    feat: di

--- YieldTrackApi/Infrastructure/IoC/DependencyInjection.cs
+++ YieldTrackApi/Infrastructure/IoC/DependencyInjection.cs
@@ -32,6 +32,7 @@ public static class DependencyInjection
 
         services.AddScoped<IUserRepository, UserRepository>();
         services.AddScoped<IAssetRepository, AssetRepository>();
+        services.AddScoped<IPriceHistoryRepository, PriceHistoryRepository>();
 
         return services;
     }
