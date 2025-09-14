5d9af1d9271a052dd54599b6f16cddfd2e1d59eb
humberto <130328321+HumbertoVitalino@users.noreply.github.com>
2025-09-13 23:09:01 -0300

    feat: upsert method and refactor model

--- YieldTrackApi/Infrastructure/Repositories/AssetRepository.cs
+++ YieldTrackApi/Infrastructure/Repositories/AssetRepository.cs
@@ -38,4 +38,36 @@ public class AssetRepository(YieldTrackContext context) : Repository<FixedIncome
     {
         await _context.AddAsync(asset.MapToModel(), cancellationToken);
     }
+
+    public async Task<FixedIncomeAsset> UpsertAsync(FixedIncomeAsset asset, CancellationToken cancellationToken)
+    {
+        var existingAsset = await _context.FixedIncomeAssets
+            .AsNoTracking()
+            .FirstOrDefaultAsync(x => x.Code == asset.Code, cancellationToken: cancellationToken);
+
+        if (existingAsset == null)
+        {
+            return await AddAssetAsync(asset, cancellationToken);
+        }
+
+        return UpdateAsset(asset, existingAsset.Id, cancellationToken);
+    }
+
+    private async Task<FixedIncomeAsset> AddAssetAsync(FixedIncomeAsset asset, CancellationToken cancellationToken)
+    {
+        var assetModel = asset.MapToModel();
+
+        await _context.FixedIncomeAssets.AddAsync(assetModel, cancellationToken);
+
+        return asset;
+    }
+
+    private FixedIncomeAsset UpdateAsset(FixedIncomeAsset asset, Guid id, CancellationToken cancellationToken)
+    {
+        var updatedAsset = asset.MapToModel(id);
+
+        _context.FixedIncomeAssets.Update(updatedAsset);
+
+        return updatedAsset.MapToDomain();
+    }
 }
