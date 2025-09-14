736b07422c6376320834ae6f8ea3079b6ed1d720
humberto <130328321+HumbertoVitalino@users.noreply.github.com>
2025-09-14 16:39:31 -0300

    feat: price controller

---
+++ YieldTrackApi/Api/Controllers/PricesController.cs
@@ -0,0 +1,21 @@
+﻿using Application.UseCases.GetPriceHistoriesByCode.Boundaries;
+using MediatR;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Api.Controllers;
+
+[ApiController]
+[Route("api/v1/[controller]")]
+public class PricesController(
+    IMediator mediator
+) : BaseController
+{
+    private readonly IMediator _mediator = mediator;
+
+    [HttpGet("{assetId}")]
+    public async Task<IActionResult> GetAsync([FromRoute] Guid assetId, CancellationToken cancellationToken)
+    {
+        var response = await _mediator.Send(new GetPriceHistoriesByCodeInput(assetId), cancellationToken);
+        return Ok(response);
+    }
+}
