7c6d489fa127ff8f6e0cf706e5d657135c4ff543
humberto <130328321+HumbertoVitalino@users.noreply.github.com>
2025-09-14 16:39:16 -0300

    feat: get prices uc

---
+++ YieldTrackApi/Application/UseCases/GetPriceHistoriesByCode/GetPriceHistoriesByCode.cs
@@ -0,0 +1,30 @@
+﻿using Application.Commons;
+using Application.Interfaces.Repositories;
+using Application.UseCases.GetPriceHistoriesByCode.Boundaries;
+using Domain;
+using MediatR;
+
+namespace Application.UseCases.GetPriceHistoriesByCode;
+
+public sealed class GetPriceHistoriesByCode(
+    IPriceHistoryRepository priceRepository
+) : IRequestHandler<GetPriceHistoriesByCodeInput, Output>
+{
+    private readonly IPriceHistoryRepository _priceRepository = priceRepository;
+
+    public async Task<Output> Handle(GetPriceHistoriesByCodeInput input, CancellationToken cancellationToken)
+    {
+        Output output = new();
+
+        var prices = await _priceRepository.GetByAssetIdAsync(input.AssetId, cancellationToken);
+
+        if (!prices.Any())
+        {
+            output.AddResult(Array.Empty<PriceHistory>());
+            return output;
+        }
+
+        output.AddResult(prices);
+        return output;
+    }
+}
